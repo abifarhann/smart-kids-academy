2c48c48a9b739232bc0ffd1ab2fc6c113daa2d47
angelvalency <angelvalency05@gmail.com>
2025-06-13 02:11:56 +0700

    dashboard partials first commit

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 
-<<<<<<< HEAD
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-=======
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
->>>>>>> bda1f2cae68512971830097d21879e1c8a58281a
 
 // Auth
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
