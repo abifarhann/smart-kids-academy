2e073a753e5aaabb06c97d071790395e756fbc79
abifarhann <abidfarhan508@gmail.com>
2025-07-01 22:01:13 +0700

    fix for main

--- resources/views/components/form-data-siswa.blade.php
+++ resources/views/components/form-data-siswa.blade.php
@@ -2,17 +2,19 @@
 <div x-data="{ pageName: `{{ isset($siswa) ? 'Edit Data Siswa' : 'Tambah Data Siswa' }}` }">
     <div class="mb-6 flex flex-wrap items-center justify-between gap-3">
         <h2 class="text-xl font-semibold text-gray-800 dark:text-white/90" x-text="pageName"></h2>
-        <nav class="flex items-center gap-2 text-gray-500 dark:text-gray-400">
+        <nav>
             <ol class="flex items-center gap-1.5 text-sm">
                 <!-- Rekapitulasi Data -->
-                <p class="flex items-center gap-1.5 text-gray-500 dark:text-gray-400">
-                    Rekapitulasi Data
-                    <svg class="stroke-current" width="17" height="16" viewBox="0 0 17 16" fill="none"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path d="M6.0765 12.667L10.2432 8.50033L6.0765 4.33366" stroke="currentColor" stroke-width="1.2"
-                            stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
-                </p>
+                <li>
+                    <a href="index.html" class="flex items-center gap-1.5 text-gray-500 dark:text-gray-400">
+                        Rekapitulasi Data
+                        <svg class="stroke-current" width="17" height="16" viewBox="0 0 17 16" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path d="M6.0765 12.667L10.2432 8.50033L6.0765 4.33366" stroke="currentColor"
+                                stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                    </a>
+                </li>
 
                 <!-- Siswa -->
                 <li>
@@ -70,15 +72,6 @@
                             value="{{ old('tgl_lahir', isset($siswa) ? \Carbon\Carbon::parse($siswa->tgl_lahir)->format('Y-m-d') : '') }}"
                             class="w-full h-11 px-4 py-2.5 pr-11 text-sm text-gray-800 placeholder:text-gray-400 bg-transparent dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 rounded-lg border border-gray-300 dark:border-gray-700 shadow-theme-xs focus:border-brand-300 dark:focus:border-brand-800 focus:ring-3 focus:ring-brand-500/10 focus:outline-none dark:bg-dark-900"
                             aria-describedby="tgl_lahir_help" onclick="this.showPicker()">
-                        <span
-                            class="pointer-events-none absolute top-1/2 right-3 -translate-y-1/2 text-gray-500 dark:text-gray-400">
-                            <svg class="fill-current" width="20" height="20" viewBox="0 0 20 20" fill="none"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" clip-rule="evenodd"
-                                    d="M6.66659 1.5415C7.0808 1.5415 7.41658 1.87729 7.41658 2.2915V2.99984H12.5833V2.2915C12.5833 1.87729 12.919 1.5415 13.3333 1.5415C13.7475 1.5415 14.0833 1.87729 14.0833 2.2915V2.99984L15.4166 2.99984C16.5212 2.99984 17.4166 3.89527 17.4166 4.99984V7.49984V15.8332C17.4166 16.9377 16.5212 17.8332 15.4166 17.8332H4.58325C3.47868 17.8332 2.58325 16.9377 2.58325 15.8332V7.49984V4.99984C2.58325 3.89527 3.47868 2.99984 4.58325 2.99984L5.91659 2.99984V2.2915C5.91659 1.87729 6.25237 1.5415 6.66659 1.5415ZM6.66659 4.49984H4.58325C4.30711 4.49984 4.08325 4.7237 4.08325 4.99984V6.74984H15.9166V4.99984C15.9166 4.7237 15.6927 4.49984 15.4166 4.49984H13.3333H6.66659ZM15.9166 8.24984H4.08325V15.8332C4.08325 16.1093 4.30711 16.3332 4.58325 16.3332H15.4166C15.6927 16.3332 15.9166 16.1093 15.9166 15.8332V8.24984Z"
-                                    fill=""></path>
-                            </svg>
-                        </span>
                     </div>
                 </div>
 
@@ -108,25 +101,38 @@
                 </div>
             </div>
 
-        <!-- Elements -->
-        <div class="flex justify-start gap-4 items-center">
-            <div>
-                <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
-                    Nama Wali
-                </label>
-                <input type="text" placeholder="Masukkan nama wali siswa"
-                    class="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30">
-            </div>
+            <!-- Baris 2 -->
+            <div class="flex justify-start gap-4 items-center">
+                <div>
+                    <label for="id_user" class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
+                        Nama Wali
+                    </label>
+                    <select name="id_user" id="id_user"
+                        class="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 h-11 w-full rounded-lg border border-gray-300 px-4 py-2.5 text-sm text-gray-800 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90">
+                        <option value="" disabled
+                            {{ old('id_user', $siswa->id_user ?? '') == '' ? 'selected' : '' }}>
+                            Pilih Wali
+                        </option>
+                        @foreach ($dataWali as $id => $nama)
+                            <option value="{{ $id }}"
+                                {{ old('id_user', $siswa->id_user ?? '') == $id ? 'selected' : '' }}>
+                                {{ $nama }}
+                            </option>
+                        @endforeach
+                    </select>
+                    @error('id_user')
+                        <p class="mt-1 text-sm text-red-600">{{ $message }}</p>
+                    @enderror
+                </div>
 
-            {{-- No WA --}}
-            <div>
-                <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
-                    Nomor WA
-                </label>
-                <input type="text" placeholder="Masukkan nomor wa"
-                    class="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 h-11 w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 placeholder:text-gray-400 focus:ring-3 focus:outline-hidden dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30">
-            </div>
-        </div>
+                <div>
+                    <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
+                        Nomor WA
+                    </label>
+                    <input type="number" name="phone" placeholder="Masukkan nomor WA"
+                        value="{{ old('phone', $siswa->phone ?? '') }}"
+                        class="dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 h-11 w-full rounded-lg border border-gray-300 px-4 py-2.5 text-sm text-gray-800 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90">
+                </div>
 
                 <div>
                     <label for="tgl_mulai" class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
@@ -137,15 +143,6 @@
                             value="{{ old('tgl_mulai', isset($siswa) ? \Carbon\Carbon::parse($siswa->tgl_mulai)->format('Y-m-d') : '') }}"
                             class="w-full h-11 px-4 py-2.5 pr-11 text-sm text-gray-800 placeholder:text-gray-400 bg-transparent dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30 rounded-lg border border-gray-300 dark:border-gray-700 shadow-theme-xs focus:border-brand-300 dark:focus:border-brand-800 focus:ring-3 focus:ring-brand-500/10 focus:outline-none dark:bg-dark-900"
                             aria-describedby="tgl_mulai_help" onclick="this.showPicker()">
-                        <span
-                            class="pointer-events-none absolute top-1/2 right-3 -translate-y-1/2 text-gray-500 dark:text-gray-400">
-                            <svg class="fill-current" width="20" height="20" viewBox="0 0 20 20"
-                                fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" clip-rule="evenodd"
-                                    d="M6.66659 1.5415C7.0808 1.5415 7.41658 1.87729 7.41658 2.2915V2.99984H12.5833V2.2915C12.5833 1.87729 12.919 1.5415 13.3333 1.5415C13.7475 1.5415 14.0833 1.87729 14.0833 2.2915V2.99984L15.4166 2.99984C16.5212 2.99984 17.4166 3.89527 17.4166 4.99984V7.49984V15.8332C17.4166 16.9377 16.5212 17.8332 15.4166 17.8332H4.58325C3.47868 17.8332 2.58325 16.9377 2.58325 15.8332V7.49984V4.99984C2.58325 3.89527 3.47868 2.99984 4.58325 2.99984L5.91659 2.99984V2.2915C5.91659 1.87729 6.25237 1.5415 6.66659 1.5415ZM6.66659 4.49984H4.58325C4.30711 4.49984 4.08325 4.7237 4.08325 4.99984V6.74984H15.9166V4.99984C15.9166 4.7237 15.6927 4.49984 15.4166 4.49984H13.3333H6.66659ZM15.9166 8.24984H4.08325V15.8332C4.08325 16.1093 4.30711 16.3332 4.58325 16.3332H15.4166C15.6927 16.3332 15.9166 16.1093 15.9166 15.8332V8.24984Z"
-                                    fill=""></path>
-                            </svg>
-                        </span>
                     </div>
                 </div>
             </div>
