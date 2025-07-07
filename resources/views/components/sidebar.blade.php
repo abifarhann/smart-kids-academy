5eeea8c06ef6e144371329f4e44d0b8aa485c3f1
Firsty Angelica Valency <149746443+angelvalency@users.noreply.github.com>
2025-07-07 14:59:37 +0700

    Frontend (#5)
    * fix dashboard
    
    * nothing
    
    * setup database
    
    * feat (auth) : add login regis
    
    * fix (auth) : fix route auth
    
    * -
    
    * dashboard partials first commit
    
    * add page kelola data siswa, mentor dan akun walimurid
    
    * feat (wali murid) : crud wali murid, fix alert and header
    
    * nothing
    
    * add penilaian bulanan fitures
    
    * add penilaian semester fitures, fixing nav
    
    * feat(siswa): add crud siswa
    
    * feat(mentor) : fix crud mentor
    
    * fixing interface
    
    * merge branch
    
    * view wali
    
    * wali murid
    
    * remove email akun wali
    
    * initate mapel
    
    * done
    
    * done gak kanggg??
    
    * done gak kanggg??
    
    * done btn
    
    ---------
    
    Co-authored-by: abifarhann <abidfarhan508@gmail.com>

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -381,7 +381,7 @@
                         Panel admin untuk pengelolaan data siswa, mapel dan tentor
                     </p>
                     <a href="{{route('web')}}"
-                        class="flex items-center justify-center rounded-lg bg-brand-500 p-3 text-theme-sm font-medium text-white hover:bg-brand-600">
+                        class="flex items-center justify-center rounded-lg p-3 text-theme-sm font-medium text-white hover:bg-brand-600"  style="background-color: #0047A0;">
                         Kembali ke Beranda
                     </a>
                 </div>
@@ -398,7 +398,9 @@
                         Platform pemantauan hasil belajar siswa untuk mendukung evaluasi pendidikan yang lebih efektif.
                     </p>
                     <a href="{{route('web')}}"
-                        class="flex items-center justify-center rounded-lg bg-brand-500 p-3 text-theme-sm font-medium text-white hover:bg-brand-600">
+
+                        class="flex items-center justify-center rounded-lg p-3 text-theme-sm font-medium text-white hover:bg-brand-600" style="background-color: #0047A0;">
+
                         Kembali ke Beranda
                     </a>
                 </div>
